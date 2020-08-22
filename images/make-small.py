4ad5993cd6e8c833e19ed7c87a3a25043f67cf6e
mityinzer <mwill@mit.edu>
2020-08-22 18:21:23 +1000

    adding small images

---
+++ images/make-small.py
@@ -0,0 +1,7 @@
+import os
+
+for filename in os.listdir("."):
+    if not filename.startswith('photo'): continue
+    cmd = 'sips -Z 300 --out small-%s.jpg %s' % (filename[:-4],filename)
+    print cmd
+    os.system(cmd)
