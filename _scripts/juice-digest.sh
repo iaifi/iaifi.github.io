2e8bbe0141a2f2d746550c9c9104c3f8355e8d6e
Jesse Thaler <jthaler@mit.edu>
2024-03-28 16:15:58 -0400

    Testing out new digest

--- _scripts/juice-digest.sh
+++ _scripts/juice-digest.sh
@@ -7,6 +7,8 @@ NC='\033[0m'
 echo -e "\n${BLUE}building site...${NC}\n"
 bundle exec jekyll build
 echo -e "\n${BLUE}inlining styles...${NC}"
-juice --preserve-important true --remove-style-tags false --web-resources-images false _site/hidden-digest-testing.html _site/hidden-digest-testing-juiced.html
-pbcopy < _site/hidden-digest-testing-juiced.html
+mkdir -p _site/digest/assets/css/
+cp _site/assets/css/main.css _site/digest/assets/css/main.css
+juice --preserve-important true --remove-style-tags false --web-resources-images false _site/digest/2024-04-01.html _site/digest/2024-04-01-juiced.html
+pbcopy < _site/digest/2024-04-01-juiced.html
 echo -e "\n${BLUE}html copied to clipboard 🌿${NC}\n"
