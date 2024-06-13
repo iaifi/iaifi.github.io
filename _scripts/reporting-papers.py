5b71a1cdb3aa50ae88f2b4b9ebfbc7cff13830b8
Jesse Thaler <jthaler@mit.edu>
2024-06-13 16:57:11 -0400

    reporting by quarter

--- _scripts/reporting-papers.py
+++ _scripts/reporting-papers.py
@@ -3,6 +3,35 @@
 # importing modules
 import yaml,os
 
+def iaifi_quarter(month):
+  if month in ['10','11','12']: return 'Q1'
+  if month in ['01','02','03']: return 'Q2'
+  if month in ['04','05','06']: return 'Q3'
+  if month in ['07','08','09']: return 'Q4'
+  raise Exception("Quarter not defined for:" + month)
+
+def iaifi_year(year,month):
+  if month in ['10','11','12']:
+    if year == '2019': return 'Y0'
+    if year == '2020': return 'Y1'
+    if year == '2021': return 'Y2'
+    if year == '2022': return 'Y3'
+    if year == '2023': return 'Y4'
+    if year == '2024': return 'Y5'
+  else:
+    if year == '2020': return 'Y0'
+    if year == '2021': return 'Y1'
+    if year == '2022': return 'Y2'
+    if year == '2023': return 'Y3'
+    if year == '2024': return 'Y4'
+    if year == '2025': return 'Y5'
+
+  raise Exception("Year not defined for:" + year + "_" + month)
+
+def iaifi_year_quarter(year,month):
+  return iaifi_year(year,month) + "_" + iaifi_quarter(month)
+
+
 # importing products
 products = os.listdir("../_data/products/")
 products.sort(reverse=True)
@@ -11,13 +40,13 @@ outputfiledictionary = {}
 outputcountdictionary = {}
 
 # Creating all of the output files
-for thrust in ['T','E','A']:
+for thrust in ['T','E','A','F']:
   for status in ['submitted','published']:
-    for year in ['2020','2021','2022','2023','2024']:
-      for month in ['01','02','03','04','05','06','07','08','09','10','11','12']:
+    for year in ['Y0','Y1','Y2','Y3','Y4','Y5']:
+      for quarter in ['Q1','Q2','Q3','Q4']:
 
         # creating TeX file
-        datename = year + "-" + month
+        datename = year + "_" + quarter
         outputfilename = 'papers_'+thrust+'_'+status+'_'+datename+'.tex'
         outputfile = open('reporting/'+outputfilename,"w")
         outputcount = 0
@@ -41,9 +70,10 @@ for filename in products:
 
   # figure out which file to put things in
   thrust = parsed['iaifi-thrust'][0]
-  submitteddate = filename[0:7]
+  submitteddate = iaifi_year_quarter(filename[0:4],filename[5:7])
   publisheddate = ''
-  if ('publication-date' in parsed): publisheddate = parsed['publication-date'][0:7]
+  if ('publication-date' in parsed and not parsed['publication-date'] == ''):
+    publisheddate = iaifi_year_quarter(parsed['publication-date'][0:4],parsed['publication-date'][5:7])
   
   # first do submitted
   status = 'submitted'
@@ -81,12 +111,12 @@ for filename in products:
   outputfile.write(paper+'\n')
 
 # Closing all of the output files
-for thrust in ['T','E','A']:
+for thrust in ['T','E','A','F']:
   for status in ['submitted','published']:
-    for year in ['2020','2021','2022','2023','2024']:
-      for month in ['01','02','03','04','05','06','07','08','09','10','11','12']:
+    for year in ['Y0','Y1','Y2','Y3','Y4','Y5']:
+      for quarter in ['Q1','Q2','Q3','Q4']:
 
-        datename = year + "-" + month
+        datename = year + "_" + quarter
         outputfilename = 'papers_'+thrust+'_'+status+'_'+datename+'.tex'
         outputfile = outputfiledictionary[outputfilename]
         outputcount = outputcountdictionary[outputfilename]
